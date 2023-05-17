94dec539bcb9589b9106d226369613ff2988caee
FluffaTheDuck <cbp@posche.dk>
2023-05-17 11:56:01 +0200

    something

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -12,8 +12,12 @@ const Content = () => {
   const [selectedClass, setSelectedClass] = useState<Class>();
   const [selectedSpec, setSelectedSpec] = useState<Spec>();
   const [selectedDungeon, setSelectedDungeon] = useState<string[]>([]);
-  const [selectedGear, setSelectedGear] = useState<Map<string, boolean>>(new Map());
-  const [statPriority, setStatPriority] = useState<Map<string, number>>(new Map());
+  const [selectedGear, setSelectedGear] = useState<Map<string, boolean>>(
+    new Map()
+  );
+  const [statPriority, setStatPriority] = useState<Map<string, number>>(
+    new Map()
+  );
   const [result, setResult] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -45,21 +49,35 @@ const Content = () => {
       >
         <div className="flex flex-col w-1/3">
           <StatPrior mainStat={mainStat} setStatPriority={setStatPriority} />
-          <DungeonRaidSelect selectedDungeon={selectedDungeon} setSelectedDungeon={setSelectedDungeon} />
+          <DungeonRaidSelect
+            selectedDungeon={selectedDungeon}
+            setSelectedDungeon={setSelectedDungeon}
+          />
         </div>
         <div className="flex flex-col w-1/3">
           <GearSelect setSelectedGear={setSelectedGear} />
-          {isLoading && <div className="flex justify-center">
-            <img src="https://thumbs.gfycat.com/IlliterateConfusedAmericancrayfish-small.gif" alt="loading" className="" />{" "}
-          </div>}
+          {isLoading && (
+            <div className="flex justify-center">
+              <img
+                src="https://thumbs.gfycat.com/IlliterateConfusedAmericancrayfish-small.gif"
+                alt="loading"
+                className=""
+              />{" "}
+            </div>
+          )}
         </div>
         <div className="flex flex-col w-1/3">
           <Result />
           <div className="pb-5 flex flex-col items-center">
             <div>
-          <button className="bg-green-500 py-1 px-3 border-b-2 border-green-700 hover:border-b-0 rounded-lg" onClick={handleRunCalculations}>Run Calculations</button>
-          </div>
-          <p>{result && result}</p>
+              <button
+                className="bg-green-500 py-1 px-3 border-b-2 border-green-700 hover:border-b-0 rounded-lg"
+                onClick={handleRunCalculations}
+              >
+                Run Calculations
+              </button>
+            </div>
+            <p>{result && result}</p>
           </div>
         </div>
       </div>
